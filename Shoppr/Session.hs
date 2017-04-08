e03ee16352898e8483ce87097bd2f3831da4bc66
PurduePL <rahmank@purdue.edu>
2017-04-08 19:42:18 -0400

    servers fixed

--- Shoppr/Session.hs
+++ Shoppr/Session.hs
@@ -54,10 +54,10 @@ beginSession ns = do
 
 endSession :: Session -> IO ()
 endSession s = do
-  threadDelay $ 5000000
-  let req = encode $ Request cTABLE_NAME DropSessID (s^.sessid) 0
-  liftIO $ ZMQ4.send (s^.server) [] req
-  responseBlob <- liftIO $ ZMQ4.receive (s^.server)
+  threadDelay $ 500000
+  --let req = encode $ Request cTABLE_NAME DropSessID (s^.sessid) 0
+  --liftIO $ ZMQ4.send (s^.server) [] req
+  --responseBlob <- liftIO $ ZMQ4.receive (s^.server)
   ZMQ4.disconnect (s ^. server) (s^.serverAddr)
 
 runSession :: Show a =>  NameService -> CSN a -> IO a
