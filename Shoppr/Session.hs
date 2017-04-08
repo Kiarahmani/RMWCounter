1626a01832a30a07bd6b3b0f745351a2a30b75fe
PurduePL <rahmank@purdue.edu>
2017-04-08 19:23:25 -0400

    servers fixed

--- Shoppr/Session.hs
+++ Shoppr/Session.hs
@@ -46,9 +46,9 @@ beginSession :: NameService -> IO Session
 beginSession ns = do
   (serverAddr, sock) <- getClientJoin ns
   sessid <- SessID <$> randomIO
-  let req = encode $ Request cTABLE_NAME AddSessID sessid 0
-  liftIO $ ZMQ4.send sock [] req
-  responseBlob <- liftIO $ ZMQ4.receive sock
+  --let req = encode $ Request cTABLE_NAME AddSessID sessid 0
+  --liftIO $ ZMQ4.send sock [] req
+  --responseBlob <- liftIO $ ZMQ4.receive sock
   threadDelay 6000000
   return $ Session (getFrontend ns) sock serverAddr sessid M.empty
 
