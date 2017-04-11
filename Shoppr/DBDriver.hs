578944ef9c5fbcd16b0a3cf54a983b49f65e0d79
PurduePL <rahmank@purdue.edu>
2017-04-10 20:42:23 -0400

    commit

--- Shoppr/DBDriver.hs
+++ Shoppr/DBDriver.hs
@@ -161,7 +161,7 @@ dropLockTable tname = do
 
 tryGetLock :: TableName -> Cas Bool 
 tryGetLock tname = do 
-  res <- executeTrans (mkLockUpdate tname) (0,False) ALL
+  res <- executeTrans (mkLockUpdate tname) (0,False) SERIAL
   if res 
   then return True
   else do
@@ -179,7 +179,7 @@ getLock tname = do
 
 releaseLock :: TableName -> Cas ()
 releaseLock tname = do 
-  res <- executeTrans (mkLockUpdate tname) (0,True) ALL
+  res <- executeTrans (mkLockUpdate tname) (0,True) SERIAL
   return ()
 
 
