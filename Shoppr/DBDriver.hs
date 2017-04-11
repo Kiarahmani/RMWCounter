6cdcf5acd99a165e68b0ea72bcee947dc1c1de88
PurduePL <rahmank@purdue.edu>
2017-04-10 21:15:11 -0400

    commit

--- Shoppr/DBDriver.hs
+++ Shoppr/DBDriver.hs
@@ -148,7 +148,7 @@ dropSessID tname sid = do
 initLock :: TableName -> Cas Bool 
 initLock tname = do 
   liftIO . print =<< executeSchema ALL (mkCreateLockTable tname) ()
-  res <- executeTrans (mkLockUpdate tname) (0,True) ALL
+ -- res <- executeTrans (mkLockUpdate tname) (0,True) ALL
   if res
   then return True
   else error $ "initialization falied"
