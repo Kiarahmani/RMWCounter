fb83b9b5382764ea8d1e9c10a04c5e4045fa87eb
PurduePL <rahmank@purdue.edu>
2017-04-13 03:15:18 -0400

    final

--- Shoppr/Session.hs
+++ Shoppr/Session.hs
@@ -49,7 +49,7 @@ beginSession i  ns = do
   let req = encode $ Request cTABLE_NAME AddSessID sessid 0
   liftIO $ ZMQ4.send sock [] req
   responseBlob <- liftIO $ ZMQ4.receive sock
-  threadDelay $ 3000000 
+  threadDelay $ 3500000 
   return $ Session (getFrontend ns) sock serverAddr sessid M.empty
 
 endSession :: Session -> IO ()
